d71d2ad5fe161d49c40be0402ab8799218395521
Benedict Arhin <benedict.arhin@amalitech.com>
2023-06-06 13:52:06 +0000

    worked on customer reviews

---
+++ frontend/src/components/CustomerReviews.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const CustomerReviews = () => {
+  return (
+    <div className='border border-black mx-4'>
+    <div className='text-2xl font-semibold text-center'>Trusted By Thousands of Happy Customers</div>
+    {/* Reviews */}
+    <div>
+        
+    </div>
+    </div>
+  )
+};
