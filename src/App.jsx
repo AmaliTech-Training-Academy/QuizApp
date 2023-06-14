7f806000d2e3524b5409ffcb4b46007016d56039
kevin68107 <kevin.aihoon@amalitech.com>
2023-06-14 16:33:58 +0000

    created the forget pssword component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import LandingPage from './pages/LandingPage'
+// import LandingPage from './pages/LandingPage'
+import ForgetPassword from './pages/ForgetPassword'
 import './App.css'
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LandingPage />} />
+          <Route path="/" element={<ForgetPassword />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/ForgetPassword.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ForgotHero from '../components/Forgot-hero'
+
+const ForgetPassword = () => {
+  return (
+    <div>
+      <ForgotHero/>
+    </div>
+  )
+}
+
+export default ForgetPassword
