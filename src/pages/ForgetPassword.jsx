509fa7ad1d5ef1c739746b8b2643265b94d01bd2
kevin68107 <kevin.aihoon@amalitech.com>
2023-06-15 11:53:10 +0000

    Worked on the view

--- src/pages/ForgetPassword.jsx
+++ src/pages/ForgetPassword.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ForgotHero from '../components/Forgot-hero'
+import ForgotPass from '../components/ForgotPass'
 
 const ForgetPassword = () => {
   return (
     <div>
       <ForgotHero/>
-      
+      <ForgotPass/>
     </div>
   )
 }
