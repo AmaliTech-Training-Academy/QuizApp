8e7736dbbd1a758171eccc07bc2be6725c13f60b
kevin68107 <kevin.aihoon@amalitech.com>
2023-06-20 13:26:41 +0000

    Added the navigation to resetpassword

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LandingPage from './pages/LandingPage'
 import ForgetPassword from './pages/ForgetPassword'
+import ResetPassword from './pages/ResetPassword'
 import './App.css'
 import SignupPage from "./pages/signuppage/SignupPage";
+import { ToastContainer } from 'react-toastify';
 
 function App() {
 
@@ -13,7 +15,9 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="signup" element={<SignupPage/>}/>
           <Route path="forgetpassword" element={<ForgetPassword/>}/>
+          <Route path="resetPassword/:id/:token" element={<ResetPassword/>}/>
         </Routes>
+        <ToastContainer/>
       </BrowserRouter>
     </>
   )
