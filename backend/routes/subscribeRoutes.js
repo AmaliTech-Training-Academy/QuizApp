d629f013df2770bbdb8b51987f7ec8896eca4847
kevin68107 <kevin.aihoon@amalitech.com>
2023-06-29 02:17:46 +0000

    worked on the passedscore for the feedback on Quizzes

--- backend/routes/subscribeRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const subscribeUser = require("../controller/subscribeController");
-const subscribe = require('../models/subscribeModel')
-
-const router = express.Router();
-
-router.post("/", subscribe, subscribeUser);
-
-module.exports = router;
