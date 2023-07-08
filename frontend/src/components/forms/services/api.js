e37fb6ce754cd1f90f69ee850c893482c9eb211e
Gloria-Abedi-Gyemfa <gloria.gyemfa@amalitech.com>
2023-07-08 13:24:27 +0000

    fixed token headers

--- frontend/src/components/forms/services/api.js
+++ frontend/src/components/forms/services/api.js
@@ -1,4 +1,7 @@
 import axios from 'axios'
+import Cookies from 'js-cookie'
+
+const token = Cookies.get('rememberMe');
 const Api = axios.create({
   baseURL: 'https://quiz-master.onrender.com/api/',
   headers: {Authorization: token ? `Bearer ${token}` : null}
