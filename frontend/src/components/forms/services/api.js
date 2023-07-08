a7493ec35e33723f3f4ae7edd78dc30ff6e32fd3
Gloria-Abedi-Gyemfa <gloria.gyemfa@amalitech.com>
2023-07-08 13:10:37 +0000

    fixed token headers

--- frontend/src/components/forms/services/api.js
+++ frontend/src/components/forms/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 const Api = axios.create({
   baseURL: 'https://quiz-master.onrender.com/api/',
+  headers: {Authorization: token ? `Bearer ${token}` : null}
 })
 
 export default Api
