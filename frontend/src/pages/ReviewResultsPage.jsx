abfeaf5d8a70eafe4f66b1436baf9a892a4ae2c3
Kevinaihoon <44543949+Kevinaihoon@users.noreply.github.com>
2023-07-13 16:51:20 +0000

    worked on the results page

--- frontend/src/pages/ReviewResultsPage.jsx
+++ frontend/src/pages/ReviewResultsPage.jsx
@@ -15,14 +15,17 @@ const ReviewResultsPage = () => {
     const topics = useSelector((state) => state.topics.data);
 
     const answers = useSelector((state) => state.answers.resultsData[0]);
-    const score = useSelector((state) => state.answers.resultsData[0].score);
+    const score = answers.score;
     console.log(score) 
     // const result = answers.filter(answer => answer[0] )
     console.log(answers.results);
 
     const quiz = topics.filter(topic => id === topic._id);
 
-    
+    // const pass = {
+    //     color: "#FFFFFF",
+    // };
+
     useEffect(()=>{
         dispatch(getResults({userId: userId, quizId: id, token:token}))
     }, [userId, id, token])
@@ -43,10 +46,12 @@ const ReviewResultsPage = () => {
         {/* Score */}
         <div className='flex justify-between bg-[#F0F2F4] px-16 py-6 mb-20'>
             <div>
-                <p className='text-[#1d2939] font-semibold text-4xl'>Congratulations! You passed!</p>
-                <p className='text-[#1d2939] font-semibold text-3xl'>Grade received {score}% To pass 80% or higher</p>
+                <p className='text-[#1d2939] font-semibold text-4xl'>{score >= 40 ? 'Congratulations! You passed!' : 'Sorry! you failed!'}</p>
+                <p className='text-[#1d2939] font-semibold text-3xl'>Grade received <span style={score >= 40 ? {color: '#3f3'} : {color: 'red'}}>{score}%</span> To pass 80% or higher</p>
             </div>
+            <NavLink to='/quizzes'>
             <button className='bg-[#0267FF] px-8 py-3'>Next Item</button>
+            </NavLink>
         </div>
         {/* Results */}
         <div className='flex flex-col items-center justify-center'>
@@ -54,13 +59,15 @@ const ReviewResultsPage = () => {
             {answers.results && answers.results.map((result, index) => (
                 <div key={index}>
                     {/* Render the individual result */}
-                    <p>Question: {result.question}</p>
-                    <p className='my-5 py-5 px-3 border-2 rounded-lg border-[#3f3] bg-[#cfc]'>Answer: {result.chosenAnswer}</p>
+                    <p>{result.questionNumber}. {result.question}</p>
+                    <p className='my-5 py-5 px-3 rounded-lg ' style={result.correctAnswer === result.chosenAnswer ? {color: 'green', borderColor: '#3f3', borderWidth: '2px', background: '#cfc'} : {color: 'red', borderColor: 'red', borderWidth: '2px', background: '#FFE6E6'}}>Answer:{result.chosenAnswer}</p>
                 </div>
                 ))}
             </div>
             <div className='my-5'>
+                <Link to={`/quiz/${id}/quizintro`}>
                 <button className='bg-[#0267FF] px-10 py-3'>Try again</button>
+                </Link>
             </div>
         </div>
     </div>
