b7cd0992a1371bc98032e95a063a50a3bf67682f
Wisdom Eleeza <wisdom.eleeza@amalitech.com>
2023-07-14 16:12:02 +0000

    Fix error in the submit function

--- backend/controller/getResults.js
+++ backend/controller/getResults.js
@@ -1,4 +1,6 @@
 const QuizResult = require("../models/quizResultModel");
+const quizModel = require("../models/quizModel");
+const { userModel } = require("../models/userModels");
 
 // @desc Get Quiz Results for a User and Quiz
 // @route GET /api/users/:userId/quizzes/:quizId/results
@@ -26,9 +28,10 @@ const getQuizResults = async (req, res) => {
       );
       const answers = question.answers.map((answer) => ({
         text: answer.text,
-        is_correct: answer.text === result.chosenAnswer ? true : answer.is_correct,
+        is_correct:
+          answer.text === result.chosenAnswer ? true : answer.is_correct,
         is_chosen: answer.text === result.chosenAnswer ? true : false,
-        points: answer.points
+        points: answer.points,
       }));
 
       return {
@@ -36,11 +39,15 @@ const getQuizResults = async (req, res) => {
         questionNumber: result.questionNumber,
         question: result.question,
         answers: answers,
-        points: question.points
+        points: question.points,
       };
     });
 
-    res.status(200).json({ success: true, score, results: updatedResults });
+    res.status(200).json({
+      success: true,
+      score,
+      results: updatedResults,
+    });
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, message: "Something Went Wrong" });
---
+++ backend/models/QuizLogModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose")
+const { Schema } = mongoose
+
+const quizLogSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    quizId: {
+        type: Schema.Types.ObjectId,
+        ref: "Quiz",
+    },
+    score: {
+        type: Schema.Types.Decimal128,
+        default: 0,
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const quizLogModel = mongoose.model("QuizLog", quizLogSchema)
+module.exports = quizLogModel
