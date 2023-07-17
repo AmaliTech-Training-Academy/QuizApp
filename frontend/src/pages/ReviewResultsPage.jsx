527279dd73b59d06e21f9b70a3c787321bd2b819
Kevinaihoon <44543949+Kevinaihoon@users.noreply.github.com>
2023-07-17 14:07:39 +0000

    worked with the data from the get response

--- frontend/src/pages/ReviewResultsPage.jsx
+++ frontend/src/pages/ReviewResultsPage.jsx
@@ -11,25 +11,41 @@ const ReviewResultsPage = () => {
     const {id} = useParams()
     const dispatch = useDispatch()
     
-    const token = useSelector(state => state.userData.user_token);
-    const userId = useSelector(state=> state.userData.user_id);
+    const token = useSelector((state) => state.userData.user_token);
+    const userId = useSelector((state)=> state.userData.user_id);
     const topics = useSelector((state) => state.topics.data);
 
-    const answers = useSelector((state) => state.answers.resultsData[0]);
-    const score = answers.score;
-    console.log(score) 
-    // const result = answers.filter(answer => answer[0] )
-    // console.log(answers.results);
+    const answers = useSelector((state) => state.results.data);
+    // const answers = useSelector((state) => state.quizResultsId);
+
+    const answerDesignations = ['A.', 'B.', 'C.', 'D.'];
+
+    
+    console.log(answers) 
+    const score = answers.score.$numberDecimal;
+    console.log(score);
 
     const quiz = topics.filter(topic => id === topic._id);
 
-    // const pass = {
-    //     color: "#FFFFFF",
-    // };
 
     useEffect(()=>{
-        dispatch(getResults({userId: userId, quizResultsId: id, token:token}))
-    }, [userId, id, token])
+        dispatch(getResults({userId, quizResultsId: id, token}))
+    }, [dispatch,userId, id, token])
+
+    const correct = {
+        color: 'green',
+        borderColor: '#3f3',
+        borderWidth: '2px',
+        background: '#cfc',
+      }
+
+    const wrong = {
+            color: 'red',
+            borderColor: 'red',
+            borderWidth: '2px',
+            background: '#FFE6E6',
+          }
+    
 
   return (
     <div>
@@ -56,18 +72,47 @@ const ReviewResultsPage = () => {
         </div>
         {/* Results */}
         <div className='flex flex-col items-center justify-center'>
-            <div>
-                
-            {answers.results && answers.results.map((result, index) => (
-                <div key={index}>
-                    {/* Render the individual result */}
-                    <p>{result.questionNumber}. {result.question}</p>
-                    <p className='my-5 py-5 px-3 rounded-lg flex justify-evenly items-center ' style={result.correctAnswer === result.chosenAnswer ? { color: 'green', borderColor: '#3f3', borderWidth: '2px', background: '#cfc' }  : { color: 'red', borderColor: 'red', borderWidth: '2px', background: '#FFE6E6'}}>
-                        Answer:{result.chosenAnswer} {result.correctAnswer === result.chosenAnswer ? <FiCheck /> : <MdOutlineClose />}
-                    </p>
-                </div>
-                ))}
-            </div>
+        <div>
+        {answers  &&
+            answers.results.map((result, index) => (
+            <div key={index}>
+                {/* Render the individual result */}
+                <p className='mb-5 text-2xl'>
+                {result.questionNumber}. {result.question}
+                </p>
+                <div className='grid grid-cols-2 gap-x-28'>
+                {result.answers.map((answer, answerIndex) => {
+                    const letter = answerDesignations[answerIndex % answerDesignations.length];
+                    const correctAnswer = result.answers.find(answer => answer.is_correct);
+                    return (
+                    <div key={answerIndex}>
+                        <div  className='flex rounded-md items-center justify-between p-4 mb-4'
+                        style={answer.is_correct ?  correct : wrong}
+                        >{letter}
+                         {answer.text}
+                        {answer.text === correctAnswer.text ? <FiCheck /> : <MdOutlineClose />}
+                        </div>
+                    </div>
+                    );
+                })}
+        </div>
+        {/* correct answer */}
+        <p className="my-16 rounded-lg border-2 border-[#3f3] bg-[#cfc] py-5 flex justify-center items-center">
+            {result.answers.map((answer, answerIndex) => {
+                const letter = answerDesignations[answerIndex % answerDesignations.length];
+        return (
+                <span key={answerIndex}  >
+                    {answer.is_correct && answer.text ? letter : null }
+                    {answer.is_correct && answer.text}
+                </span>
+                );
+            })}
+        </p>
+      </div>
+    ))}
+</div>
+
+
             <div className='my-5'>
                 <Link to={`/quiz/${id}/quizintro`}>
                 <button className='bg-[#0267FF] px-10 py-3'>Try again</button>
