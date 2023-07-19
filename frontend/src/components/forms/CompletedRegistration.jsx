22825fbc8ea7e3fbf64f0a10e57fafd5f88467a4
Gloria-Abedi-Gyemfa <gloria.gyemfa@amalitech.com>
2023-07-19 15:59:49 +0000

    added comments

--- frontend/src/components/forms/CompletedRegistration.jsx
+++ frontend/src/components/forms/CompletedRegistration.jsx
@@ -5,11 +5,13 @@ import { NavLink } from 'react-router-dom'
 import { toast } from 'react-toastify'
 
 const CompletedRegistration = () => {
+
   return (
     <div className={styles.completedRegistration}>
       <h2 className={styles.heading} >Woohoo!</h2>
       <p className={styles.completedDescription}>Hey there! Your registration is complete, get excited for the ultimate quiz experience of your life. Let's do this!</p>
       <img src={celebrate} className={styles.img}/>
+      {/* complete registration */}
       <NavLink to='/login'>
       <button className={styles.submitBtn} onClick={()=>{toast.success('Account created successfully')}}>Continue</button>
       </NavLink>
