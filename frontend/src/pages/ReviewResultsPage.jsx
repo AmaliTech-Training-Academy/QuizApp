69342faa1b4c543947c28698275d6a0e55fe305d
Kevinaihoon <44543949+Kevinaihoon@users.noreply.github.com>
2023-07-19 15:39:29 +0000

    styled the mobile view

--- frontend/src/pages/ReviewResultsPage.jsx
+++ frontend/src/pages/ReviewResultsPage.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { NavLink, Link, useParams } from 'react-router-dom'
-import { IoIosArrowBack } from 'react-icons/io'
+import { NavLink, Link, useParams, useNavigate} from 'react-router-dom'
+// import { IoIosArrowBack } from 'react-icons/io'
 import { MdOutlineClose, MdOutlineTimer } from 'react-icons/md'
 import { getResults } from '../features/resultsSlice'
 import { FiCheck } from 'react-icons/fi'
@@ -11,26 +11,23 @@ const ReviewResultsPage = () => {
     const {id} = useParams()
 
     const dispatch = useDispatch()
+    const navigate = useNavigate()
     
     const token = useSelector((state) => state.userData.user_token);
     const userId = useSelector((state)=> state.userData.user_id);
     const topics = useSelector((state) => state.topics.data);
     const quiz = topics.filter(topic => id === topic._id);
-    const answerDesignations = ['A.', 'B.', 'C.', 'D.'];
-
-
-    // const answers = useSelector((state) => state.results.data);
     const answers = useSelector((state) => state.answers.quizResults[0].results);
-
-    console.log(answers);
-
     const score = useSelector((state) => state.answers.quizResults[0].score);
-    console.log(score);
-
-
+    
+    const answerDesignations = ['A.', 'B.', 'C.', 'D.'];
+    
     useEffect(()=>{
+        if(answers.length > 0){
         dispatch(getResults({userId, quizResultsId: id, token}))
-    }, [dispatch,userId, id, token])
+    }
+    }, [dispatch, userId, id, token, answers])
+    
 
     const correct = {
         color: 'green',
@@ -51,43 +48,52 @@ const ReviewResultsPage = () => {
         background: '#fff',
         borderWidth: '2px',
        }
+
+       useEffect(() => {
+        const timer = setTimeout(() => {
+        //   navigate(`/quiz/${id}/results`)
+        // window.location.reload()
+        }, 10000) 
     
-    //    console.log(answers.results[0].answers[0].points);
+        return () => clearTimeout(timer)
+      }, [])
+    
+    
+       console.log(answers);
 
 return (
-    <div>
+    <div className='font-Roboto'>
         {/* header */}
-        <div className='py-6 px-6 lg:py-10 lg:px-16 bg-[#0267FF] text-white flex lg:flex-row flex-col lg:justify-between font-Roboto' id='quiz-header'>
-            <div className='lg:hidden flex items-center font-normal'><IoIosArrowBack className='mr-2'/>
-                <Link>Back</Link>
-            </div>
+        <div className='py-6 px-6 lg:py-10 lg:px-16 bg-[#0267FF] text-white flex lg:flex-row flex-col-reverse lg:justify-between' id='quiz-header'>
+            {/* <div className='lg:hidden flex items-center font-normal'><IoIosArrowBack className='mr-2'/>
+            </div> */}
             <div>
-            <p className='lg:block text-2xl font-semibold'>Test your knowledge on {quiz[0].topic}</p>
+            <p className='flex flex-shrink text-2xl font-semibold'>Test your knowledge on {quiz[0].topic}</p>
             <p>Practice Quiz .1 hour</p>
             </div>
-            <div className='flex items-center text-2xl font-semibold lg:w-1/3 lg:justify-end'><MdOutlineTimer className='w-14 h-8'/> 00:00:00 </div>
+            <div className='flex items-center justify-center text-[27.65px] font-semibold lg:w-1/3 lg:justify-end'><MdOutlineTimer className='w-14 h-8'/> 00:00:00 </div>
         </div>
         {/* Score */}
-        <div className='flex justify-between bg-[#F0F2F4] px-16 py-6 mb-20'>
-            <div>
-                <p className='text-[#1d2939] font-semibold text-4xl'>{score >= 40 ? 'Congratulations! You passed!' : 'Sorry! you failed!'}</p>
-                <p className='text-[#1d2939] font-semibold text-3xl'>Grade received <span style={score >= 40 ? {color: '#3f3'} : {color: 'red'}}>{score}%</span> To pass 80% or higher</p>
+        <div className='flex lg:justify-between bg-[#F0F2F4] px-16 py-6 mb-20'>
+            <div className='text-[#1d2939] font-semibold flex flex-col relative lg:right-0 right-10 flex-shrink-0'>
+                <p className='lg:text-4xl text-2xl'>{score >= 40 ? 'Congratulations! You passed!' : 'Sorry! you failed!'}</p>
+                <p className='lg:text-3xl text-[18px] '>Grade received <span style={score >= 40 ? {color: '#3f3'} : {color: 'red'}}>{score}%</span> To pass 80% or higher</p>
             </div>
             <NavLink to='/quizzes'>
-            <button className='bg-[#0267FF] px-8 py-3'>Next Item</button>
+            <button className='bg-[#0267FF] px-8 py-3 hidden lg:block'>Next Item</button>
             </NavLink>
         </div>
         {/* Results */}
-        <div className='flex flex-col items-center justify-center'>
+        <div className='flex flex-col items-center justify-center mx-5'>
         <div>
         {answers  &&
             answers.map((result, index) => (
                 <div key={index}>
                 {/* Render the individual result */}
-                    <p className='mb-5 text-2xl'>
+                    <p className='mb-5 lg:text-2xl text-base'>
                     {result.questionNumber}. {result.question} 
                     </p>
-                <div className='grid grid-cols-2 gap-x-28'>
+                <div className='grid lg:grid-cols-2 grid-cols-1  gap-x-28'>
                     {result.answers.map((answer, answerIndex) => {
                     const letter = answerDesignations[answerIndex % answerDesignations.length];
                     const correctAnswer = result.answers.find(answer => answer.is_correct);
@@ -106,7 +112,7 @@ return (
                                 {isWrongChoice ? <MdOutlineClose /> : isChosen ? <FiCheck /> : <input type='radio' style={{borderColor: '#1D2939'}}/>}
                                 </div>
                             </div>
-                            <div className='text-lg font-semibold bg-gray-100 border-2 rounded absolute right-8'>{answer.points}/10 points</div>
+                            <div className='text-lg font-semibold bg-gray-100 border-2 rounded absolute right-20 lg:block hidden'>{answer.points}/10 points</div>
                         </>
                     );
                 })}
@@ -128,10 +134,15 @@ return (
 </div>
 
 
-            <div className='my-5'>
+            <div className='my-5 hidden lg:block '>
                 <Link to={`/quiz/${id}/quizintro`}>
                 <button className='bg-[#0267FF] px-10 py-3'>Try again</button>
                 </Link>
+            </div>
+            <div className='lg:hidden'>
+            <NavLink to='/quizzes'>
+            <button className='bg-[#0267FF] px-8 py-3 w-80'>Next Item</button>
+            </NavLink>
             </div>
         </div>
     </div>
