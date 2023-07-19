37b9c6f71505ec9702229967f351fec80532f7f7
Wisdom Eleeza <wisdom.eleeza@amalitech.com>
2023-07-19 16:32:32 +0000

    Add API for adding interest

---
+++ backend/controller/InterestController.js
@@ -0,0 +1,30 @@
+const { userModel } = require("../models/userModels");
+
+// @desc Add interests to user's interests and addInterest fields
+// @route PATCH /api/users/interest/:id
+// @access Public
+const interests = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { interests, addInterest } = req.body;
+
+    const user = await userModel.findById(id);
+
+    if (!user)
+      return res
+        .status(404)
+        .json({ success: false, message: "User Not Found" });
+
+    user.interests = interests,
+    user.addInterest = addInterest
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({ success: true, user: updatedUser });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Something Went Wrong" });
+  }
+};
+
+module.exports = interests;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,11 +2,11 @@ const express = require("express");
 const { registerUser } = require("../controller/userController");
 const validateRegisterUser = require("../middleware/validateUser");
 const editUser = require('../controller/editUser');
-const subscribeUser = require("../controller/subscribeController");
+const interests = require("../controller/InterestController");
 const router = express.Router();
 
 router.post("/", validateRegisterUser, registerUser)
 router.patch('/photo/:id', editUser);
-// router.post('/', subscribeUser)
+router.patch('/interest/:id', interests);
 
 module.exports = router;
