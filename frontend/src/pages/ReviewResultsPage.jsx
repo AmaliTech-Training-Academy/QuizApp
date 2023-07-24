e7dabb90adb9e8bd92bfa44c0172a4f8c14e2e1a
Kevinaihoon <44543949+Kevinaihoon@users.noreply.github.com>
2023-07-24 10:40:34 +0000

    worked on the results response

--- frontend/src/pages/ReviewResultsPage.jsx
+++ frontend/src/pages/ReviewResultsPage.jsx
@@ -1,8 +1,8 @@
 import { useSelector } from 'react-redux'
 import { NavLink, Link, useParams } from 'react-router-dom'
-// import { IoIosArrowBack } from 'react-icons/io'
 import { MdOutlineClose, MdOutlineTimer } from 'react-icons/md'
 import { FiCheck } from 'react-icons/fi'
+import { ThreeDots } from 'react-loader-spinner'
 
 
 const ReviewResultsPage = () => {
@@ -38,17 +38,6 @@ const ReviewResultsPage = () => {
         borderWidth: '2px',
        }
 
-    //    useEffect(() => {
-    //     const timer = setTimeout(() => {
-    //     //   navigate(`/quiz/${id}/results`)
-    //     // window.location.reload()
-    //     }, 10000) 
-    
-    //     return () => clearTimeout(timer)
-    // }, [])
-    
-    // console.log(answers);
-
 return (
     <div className='font-Roboto mb-10'>
         {/* header */}
@@ -61,6 +50,9 @@ return (
             <div className='flex items-center justify-center text-[27.65px] font-semibold lg:w-1/3 lg:justify-end'><MdOutlineTimer className='w-14 h-8'/> 00:00:00 </div>
         </div>
         {/* Score */}
+        {
+            answers.length > 0 ? (
+                <>
         <div className='flex lg:justify-between bg-[#F0F2F4] px-16 py-6 mb-20'>
             <div className='text-[#1d2939] font-semibold flex flex-col relative lg:right-0 right-10 flex-shrink-0'>
                 <p className='lg:text-4xl text-2xl'>{score >= 80 ? 'Congratulations! You passed!' : 'Sorry! you failed!'}</p>
@@ -71,8 +63,6 @@ return (
             </NavLink>
         </div>
         {/* Results */}
-        {
-            answers.length > 0 ? (
             <div className='flex flex-col items-center justify-center mx-5'>
             <div>
             {answers  &&
@@ -133,9 +123,10 @@ return (
                 <button className='bg-[#0267FF] px-8 py-3 w-80'>Next Item</button>
                 </NavLink>
                 </div>
-            </div> ) 
+            </div> </>) 
+    
             : 
-            (<div>Please wait for your results...</div>)
+            (<div className='flex items-center justify-center px-5 text-lg my-80'>Please wait for your results<ThreeDots color='#0267FF'/></div>)
         }
     </div>
   )
