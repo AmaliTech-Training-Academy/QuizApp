7fa11521a91e41931540285058da06122856b2b5
Wisdom Eleeza <wisdom.eleeza@amalitech.com>
2023-07-26 11:20:11 +0000

    Update code  on randomizing the questions to the users

--- backend/controller/questionsController.js
+++ backend/controller/questionsController.js
@@ -1,18 +1,17 @@
 const quizModel = require("../models/quizModel");
 const shuffleArray = require("../utils/shuffleArrays");
 
-// @desc Fetching Question from database
-// @route GET /api/users/questions?topicId=topicId&page=1&limit=5
+// @desc Fetching all questions from database
+// @route GET /api/users/questions?topicId=topicId
 // @access Private
 const questions = async (req, res) => {
   const { topicId } = req.query;
-  const page = parseInt(req.query.page) || 1; // current page number
-  const limit = 5; // Number of items per page
 
   try {
     let shuffledQuestions = req.session.shuffledQuestions;
     console.log("shuff::", shuffledQuestions);
     console.log("req::", req.session);
+
     if (!shuffledQuestions) {
       const fetchedData = await quizModel.findById(topicId);
       if (!fetchedData)
@@ -28,40 +27,31 @@ const questions = async (req, res) => {
 
     const totalQuestions = shuffledQuestions.length;
 
-    // checking if the requested page is not a valid number
-    if (isNaN(page)) throw new Error("Invalid page number");
+    // Fetch all the questions at once
+    const allQuestions = shuffledQuestions.map((question, index) => {
+      // Extracting the answer texts and ids from answers
+      const extractedAnswers = question.answers.map(({ text, _id }) => ({
+        text,
+        _id,
+      }));
 
-    // checking if the requested page is out of bounds
-    if (page < 1 || page > totalQuestions)
-      throw new Error("Page number out of bounds");
+      return {
+        question: question.question,
+        answers: extractedAnswers,
+      };
+    });
 
-    // Update quiz progress in session to the current question
-    // req.session.quizProgress = page;
-
-    // Fetching the current question
-    const currentQuestion = shuffledQuestions[page - 1];
-
-    // Extracting the answer texts and ids from answers
-    const extractedAnswers = currentQuestion.answers.map(({ text, _id }) => ({
-      text,
-      _id,
-    }));
-
-    const response = {
+    res.status(200).json({
       success: true,
       topicId,
-      page,
-      limit,
       totalQuestions,
-      question: currentQuestion.question,
-      answers: extractedAnswers,
-    };
-    res.status(200).json(response);
+      questions: allQuestions,
+    });
   } catch (error) {
     console.error(error);
     res
       .status(500)
-      .json({ success: false, message: "Error fetching question" });
+      .json({ success: false, message: "Error fetching questions" });
   }
 };
 
