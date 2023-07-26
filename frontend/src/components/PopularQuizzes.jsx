6c0833d82c2eb71d4c6fe03ff0f1fdd08699bee0
Benedict Arhin <benedict.arhin@amalitech.com>
2023-07-26 11:31:07 +0000

    Fixed popular quizzes(2)

--- frontend/src/components/PopularQuizzes.jsx
+++ frontend/src/components/PopularQuizzes.jsx
@@ -7,21 +7,10 @@ import { getQuizzes } from '../features/popularQuizzesSlice'
 
 
 const PopularQuizzes = () => {
-    // const [quizzes, setQuizzes] = useState([])
-
-    // useEffect(() => {
-    //     const fetchData = async()=>{
-    //         const response = await Api.get('users/popular-topics')
-    //         setQuizzes(response.data.popularTopics);
-    //         console.log(response.data.popularTopics);
-    //     }
-    //     fetchData()
-    // }, [])
     const dispatch = useDispatch();
 
     const token = useSelector(state=> state.userData.user_token)
     const quizzes = useSelector(state=>state.popularQuizzes.data.popularTopics);
-    // console.log(quizzes);
 
     useEffect(()=>{
       dispatch(getQuizzes(token))
@@ -32,11 +21,10 @@ const PopularQuizzes = () => {
         {quizzes.length > 0 && (
         <div className="lg:grid lg:grid-cols-2 gap-8 flex flex-wrap">
         {quizzes.slice(0, 4).map((element) => (
-          <div key={element.id}>
+          <div key={element._id}>
             <Link to={`/quiz/${element._id}`}>
                 <img src={element.desktopImage} alt={element.title} className="w-full h-[250px] rounded-lg" />
                 <p className="mt-2 text-xl font-semibold">{element.topic}</p>
-                <p className="text-gray-500">{element.Date}</p>
             </Link>
           </div>
         ))}
