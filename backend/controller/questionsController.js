4051adf2911e28942c1ab56e29c774aaf00f87a1
Wisdom Eleeza <wisdom.eleeza@amalitech.com>
2023-07-27 15:44:40 +0000

    Updated the questions controller function (3)

--- backend/controller/questionsController.js
+++ backend/controller/questionsController.js
@@ -16,16 +16,16 @@ const questions = async (req, res) => {
         return res.status(404).json({ message: "Quiz Not found" });
 
       const questionsArray = fetchedData.questions || [];
-
+      
       // Shuffle both the questions and their answers arrays
-      shuffledQuestions = questionsArray.map((question,) => {
+      shuffledQuestions = questionsArray.map((question) => {
         const shuffledAnswers = shuffleArray([...question.answers]);
         return {
-          ...question,
+          question: question.question, // Include the question text in the returned object
           answers: shuffledAnswers,
         };
       });
-      
+
       // store the shuffledQuestions array in the user's session
       req.session.shuffledQuestions = shuffledQuestions;
       req.session.quizProgress = 1; // Initialize quiz progress to the first question
@@ -33,32 +33,11 @@ const questions = async (req, res) => {
 
     const totalQuestions = shuffledQuestions.length;
 
-    // Fetch all the questions at once
-    const allQuestions = shuffledQuestions.map((question, index) => {
-      // Extracting the answer texts and ids from answers
-      const extractedAnswers = question.answers.map(({ text, _id }) => ({
-        text,
-        _id,
-      }));
-
-      // Log the question and its answers
-      // console.log("Question:", question.question);
-      // console.log("Answers:", extractedAnswers);
-
-      return {
-        question: question.question,
-        answers: extractedAnswers,
-      };
-    });
-
-    // Log all questions and their answers
-    // console.log("All Questions:", allQuestions);
-
     res.status(200).json({
       success: true,
       topicId,
       totalQuestions,
-      questions: allQuestions,
+      questions: shuffledQuestions,
     });
   } catch (error) {
     console.error(error);
