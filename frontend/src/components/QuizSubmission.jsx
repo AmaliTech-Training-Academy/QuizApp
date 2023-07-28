86353f379391ebff456bf9b0fc8415104d5d53c8
Benedict Arhin <benedict.arhin@amalitech.com>
2023-07-28 10:01:29 +0000

    Minor touchups on questions page(3)

--- frontend/src/components/QuizSubmission.jsx
+++ frontend/src/components/QuizSubmission.jsx
@@ -19,25 +19,29 @@ export const QuizSubmission = ({handleSure}) => {
     // console.log(userName);
 return (
     <div className='flex flex-col lg:w-8/12 mx-auto bg-white'>
+        {check === false ? (
+        <>
         <div className='font-semibold'>QuizMaster honor code <span className='text-[#0267FF]'>Learn more</span> </div>
-        <div className='flex items-start mt-2'>
-            <input 
-            type="checkbox" 
-            className='mr-2 w-6 h-6'
-            checked= {check}
-            onChange={handleChange}
-            />
-            <div onClick={handleChange} className='cursor-pointer'>
-                I {userName}, understand that submitting work that isn't my own may result in permanent failure of this quiz or deactivativation of my quiz master account                                      
-            </div>
-        </div>
-        {/* <NavLink to={`${id}/results`}> */}
-        <button 
-        className='self-center px-32 py-4 mt-8 w-full lg:w-auto bg-[#0267FF]'
-        onClick={handleSubmit}>
-        Submit
-        </button>
-        {/* </NavLink> */}
+                <div className='flex items-start mt-2'>
+                    <input 
+                    type="checkbox" 
+                    className='mr-2 w-6 h-6'
+                    checked= {check}
+                    onChange={handleChange}
+                    />
+                    <div onClick={handleChange} className='cursor-pointer'>
+                        I {userName}, understand that submitting work that isn't my own may result in permanent failure of this quiz or deactivativation of my quiz master account                                      
+                    </div>
+                </div>
+        </>) :
+            (
+                <button 
+                className='self-center px-32 py-4 w-full lg:w-auto bg-[#0267FF]'
+                onClick={handleSubmit}>
+                Submit
+                </button>
+            )
+        }
     </div>
   )
 }
